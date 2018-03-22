c44af0a2841894d1196c6d549581c0e9e9158564
Lucy Linder <lucy.derlin@gmail.com>
2018-03-22 17:02:18 +0100

    better handle various links in webview + check write storage permissions

--- app/src/main/java/ch/derlin/mybooks/AppWebviewActivity.kt
+++ app/src/main/java/ch/derlin/mybooks/AppWebviewActivity.kt
@@ -1,26 +1,22 @@
 package ch.derlin.mybooks
 
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.webkit.WebChromeClient
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.Toast
+import android.webkit.*
 import ch.derlin.mybooks.helpers.ImageDownloadManager.downloadImage
 import kotlinx.android.synthetic.main.activity_webview.*
-import java.io.File
 
 
 class AppBrowserActivity : AppCompatActivity() {
@@ -32,6 +28,19 @@ class AppBrowserActivity : AppCompatActivity() {
         }
 
     private lateinit var url: String
+    private var writeExternalStorageIsGranted = false
+
+    // keep track of the last link/image/phone (i.e. <a href>) link pressed
+    // needed because the link is detected in shouldOverrideUrlLoading but the context menu
+    // is shown in onCreateContextMenu... So we can't pass the info as a parameter
+    private var _hit: Pair<Int, String>? = null
+    private val lastHit: Pair<Int, String>
+        get() {
+            val ret = _hit ?: Pair(webview.hitTestResult.type, webview.hitTestResult.extra)
+            _hit = null
+            return ret
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,8 +54,17 @@ class AppBrowserActivity : AppCompatActivity() {
 
         url = intent.getStringExtra("url") //?: "https://www.google.com?q=1984"
 
+        checkPermissions()
+        initWebview()
+        registerForContextMenu(webview)
+        webview.loadUrl(url)
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebview() {
         webview.webChromeClient = object : WebChromeClient() {
             override fun onProgressChanged(view: WebView, progress: Int) {
+                // show loading page progress
                 working = true
                 progressBar.setProgress(progress)
                 if (progress == 100) working = false
@@ -55,10 +73,6 @@ class AppBrowserActivity : AppCompatActivity() {
 
         WebView.setWebContentsDebuggingEnabled(true)
         webview.webViewClient = object : WebViewClient() {
-
-            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-                super.onPageStarted(view, url, favicon)
-            }
 
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
@@ -70,74 +84,76 @@ class AppBrowserActivity : AppCompatActivity() {
                 progressBar!!.visibility = View.GONE
                 invalidateOptionsMenu()
             }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                request?.let {
+                    val url = it.url.toString()
+                    if (url.startsWith("http")) return super.shouldOverrideUrlLoading(view, request)
+                    val type =
+                            if (url.startsWith("tel:")) WebView.HitTestResult.PHONE_TYPE
+                            else if (url.startsWith("mailto:")) WebView.HitTestResult.EMAIL_TYPE
+                            else if (url.startsWith("data:image")) WebView.HitTestResult.IMAGE_TYPE
+                            else WebView.HitTestResult.UNKNOWN_TYPE
+                    _hit = Pair(type, url)
+                    openContextMenu(view)
+                    return true
+                }
+
+                return super.shouldOverrideUrlLoading(view, request)
+            }
         }
         webview.clearCache(true)
         webview.clearHistory()
+
         webview.settings.javaScriptEnabled = true
         webview.settings.setSupportZoom(true)
         webview.settings.builtInZoomControls = true
         webview.isHorizontalScrollBarEnabled = true
+    }
 
-        registerForContextMenu(webview)
-        webview.loadUrl(url)
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+        } else {
+            writeExternalStorageIsGranted = true
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        writeExternalStorageIsGranted = grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onCreateContextMenu(contextMenu: ContextMenu, view: View, contextMenuInfo: ContextMenu.ContextMenuInfo?) {
         super.onCreateContextMenu(contextMenu, view, contextMenuInfo)
+        val hit = lastHit
 
-        val webViewHitTestResult = webview.getHitTestResult()
-
-        if (webViewHitTestResult.getType() == WebView.HitTestResult.IMAGE_TYPE
-                || webViewHitTestResult.getType() == WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE) {
-
-            contextMenu.add(0, 1, 0, "Download image")
+        if (hit.first != WebView.HitTestResult.UNKNOWN_TYPE &&
+                hit.first != WebView.HitTestResult.EDIT_TEXT_TYPE) {
+            contextMenu.add(0, 1, 0, "Share")
                     .setOnMenuItemClickListener {
-                        val imageUrl = webViewHitTestResult.getExtra()
-                        downloadImage(imageUrl)
-                        /*
-                        try {
-                            if (imageUrl.startsWith("data")) {
-                                downloadBase64Image(imageUrl)
-                            } else {
-                                val request = DownloadManager.Request(Uri.parse(imageUrl))
-                                request.allowScanningByMediaScanner()
-                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, imageUrl.split("/").last())
-                                val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-                                downloadManager.enqueue(request)
-                                Toast.makeText(applicationContext, "Image downloaded successfully.", Toast.LENGTH_LONG).show()
-                            }
-                        } catch (e: Throwable) {
-                            Toast.makeText(applicationContext, "something went wrong...", Toast.LENGTH_SHORT).show()
-                        }
-                        false
-                    */
+                        val shareIntent = Intent(Intent.ACTION_SEND)
+                        shareIntent.type = "text/plain"
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, hit.second)
+                        startActivity(Intent.createChooser(shareIntent, "Share link using"))
+                        true
                     }
         }
-    }
 
-    private fun downloadBase64Image(imageUrl: String) {
-        val type = Regex("data:image/([^;]+).*").find(imageUrl)?.groups?.get(1)?.value
+        // try to download the image under the click (if it is actually an image)
+        if (hit.first == WebView.HitTestResult.IMAGE_TYPE
+                || hit.first == WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE) {
 
-        if (type == null || !arrayOf("png", "jpg", "jpeg", "webp").contains(type)) {
-            return
-        }
-        val compressor = when (type) {
-            "png" -> Bitmap.CompressFormat.PNG
-            "webp" -> Bitmap.CompressFormat.WEBP
-            else -> Bitmap.CompressFormat.JPEG
-        }
-
-        compressor?.let {
-            val bytes = android.util.Base64.decode(imageUrl.split(",").last(), android.util.Base64.DEFAULT)
-            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-            val externalStorage = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-            File(externalStorage, "${System.currentTimeMillis()}.${type}").outputStream().use {
-                if (bitmap.compress(compressor, 100, it))
-                    Toast.makeText(applicationContext, "Success", Toast.LENGTH_SHORT).show()
+            if (writeExternalStorageIsGranted) {
+                contextMenu.add(0, 1, 0, "Download image")
+                        .setOnMenuItemClickListener {
+                            val imageUrl = hit.second
+                            downloadImage(imageUrl)
+                        }
             }
         }
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/ch/derlin/mybooks/helpers/ImageDownloadManager.kt
+++ app/src/main/java/ch/derlin/mybooks/helpers/ImageDownloadManager.kt
@@ -1,25 +1,29 @@
 package ch.derlin.mybooks.helpers
 
-import android.app.Activity
-import android.app.DownloadManager
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.os.Environment
 import android.support.design.widget.Snackbar
+import android.support.v4.app.NotificationCompat
 import android.webkit.URLUtil
 import android.widget.Toast
+import ch.derlin.mybooks.R
 import timber.log.Timber
 import java.io.File
 
 
 object ImageDownloadManager {
 
-    private val base64Compressors = mapOf<String, Bitmap.CompressFormat>(
+    val notificationChannelId = "derlin.mybooks"
+
+    val base64Compressors = mapOf<String, Bitmap.CompressFormat>(
             "png" to Bitmap.CompressFormat.PNG,
             "jpg" to Bitmap.CompressFormat.JPEG,
             "jpeg" to Bitmap.CompressFormat.JPEG,
@@ -41,17 +45,18 @@ object ImageDownloadManager {
     fun Activity.downloadImage(url: String): Boolean {
         if (isBase64Image(url)) {
             val imageURI = downloadBase64Image(applicationContext, url)
-            imageURI?.let { uri ->
-                Snackbar.make(findViewById(android.R.id.content),
-                        "Image downloaded", Snackbar.LENGTH_SHORT)
-                        .setAction("view", { _ ->
-                            val intent = Intent(Intent.ACTION_VIEW)
-                            intent.setDataAndType(uri, "image/*")
-                            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                            if (intent.resolveActivity(packageManager) != null)
-                                startActivity(intent)
-                        })
-                        .show()
+            imageURI?.let { _ ->
+                //                Snackbar.make(findViewById(android.R.id.content),
+//                        "Image downloaded", Snackbar.LENGTH_SHORT)
+//                        .setAction("view", { _ ->
+//                            val intent = Intent(Intent.ACTION_VIEW)
+//                            intent.setDataAndType(uri, "image/*")
+//                            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+//                            if (intent.resolveActivity(packageManager) != null)
+//                                startActivity(intent)
+//                        })
+//                        .show()
+                Toast.makeText(this, "Image downloaded", Toast.LENGTH_SHORT).show()
                 return true
             }
         } else {
@@ -73,7 +78,6 @@ object ImageDownloadManager {
         request.allowScanningByMediaScanner()
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, imageUrl.split("/").last())
-
         return (ctx.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager).enqueue(request)
     }
 
@@ -88,12 +92,46 @@ object ImageDownloadManager {
 
             file.outputStream().use {
                 if (bitmap.compress(base64Compressors.get(type), 100, it))
-                    // see https://proandroiddev.com/sharing-files-though-intents-are-you-ready-for-nougat-70f7e9294a0b
+                // see https://proandroiddev.com/sharing-files-though-intents-are-you-ready-for-nougat-70f7e9294a0b
                     return if (VERSION.SDK_INT >= VERSION_CODES.N)
                         android.support.v4.content.FileProvider.getUriForFile(ctx, ctx.packageName + ".provider", file)
                     else
                         Uri.fromFile(file)
             }
+
+            //Tell the media scanner about the new file so that it is immediately available to the user.
+            MediaScannerConnection.scanFile(ctx, arrayOf(file.toString()), null) { path, uri ->
+                Timber.d("Scanned $path:")
+                Timber.i("-> uri=$uri")
+            }
+
+            // Show a "download complete" notification
+            val intent = Intent()
+            intent.action = android.content.Intent.ACTION_VIEW
+            intent.setDataAndType(Uri.fromFile(file), "$type/*")
+            val pIntent = PendingIntent.getActivity(ctx, 0, intent, 0)
+
+            val notificationManager = ctx.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationBuilder = NotificationCompat.Builder(ctx, notificationChannelId)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentText("Downloa complete.")
+                    .setContentTitle(file.name)
+                    .setContentIntent(pIntent)
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                /* for android O (8), channel mandatory */
+                val channelName = ctx.packageName
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val channel = NotificationChannel(notificationChannelId, channelName, importance)
+                notificationManager.createNotificationChannel(channel)
+                notificationBuilder.setChannelId(notificationChannelId)
+            }
+
+            val notification = notificationBuilder.build()
+            notification.flags = notification.flags or Notification.FLAG_AUTO_CANCEL
+            val notificationId = 85851
+
+            notificationManager.notify(notificationId, notification)
         }
         return null
     }
